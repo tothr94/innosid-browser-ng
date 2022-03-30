3fa8787a9199a5113e20134fa68b2eb557cf92ac
Tóth Róbert <tothr94@gmail.com>
2022-03-30 12:34:16 +0200

    Add initial state of the browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'innosid-browser-ng';
+  names: string[] | undefined;
+  selected: string | undefined;
+
+  constructor(
+    private httpClient: HttpClient) {
+  }
+
+  getNames(): Observable<string[]> {
+    return this.httpClient.get<string[]>('http://localhost:8080');
+  }
+
+  getModelURI(): string {
+    return `http://localhost:8080/${this.selected}`
+  }
+
+  ngOnInit(): void {
+    this.getNames()
+      .subscribe({
+        next: v => this.names = v,
+        error: e => alert(e)
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
